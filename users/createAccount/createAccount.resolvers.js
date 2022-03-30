8de607910e783833b7c78e51e300a0a5a82a9f21
hyhy425 <hyhy425@naver.com>
2022-03-30 17:18:13 +0900

    2

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { username, email, name, location, password, avatarURL, githubUsername }
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username/email is already taken.");
+        }
+
+        const uglyPassword = await bcrypt.hash(password, 10);
+
+        return client.user.create({
+          data: {
+            username,
+            email,
+            name,
+            location,
+            password: uglyPassword,
+            avatarURL,
+            githubUsername,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
+    },
+  },
+  Query: {
+    seeProfile: (_, { username }) =>
+      client.user.findUnique({
+        where: {
+          username,
+        },
+      }),
+  },
+};
