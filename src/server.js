6e2088c4963ce0fa31fc0bbf380d5e8900a332df
hyhy425 <hyhy425@naver.com>
2022-04-07 12:40:30 +0900

    no playground

--- src/server.js
+++ src/server.js
@@ -9,8 +9,6 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  playground: true,
-  introspection: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
